8d9f4108a61a765e56e09b92552a54a89eb11b54
Matrks <45499570+Matrks@users.noreply.github.com>
2019-03-19 22:05:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,3 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
 
 const category = "556086713714212885";
 const devs     = ["429335711267815424"];
